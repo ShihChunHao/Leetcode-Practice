69ef095b5e447e396ee9e4072b4fbef4bc9db4bb
AaronShih <86563074+ShihChunHao@users.noreply.github.com>
2024-04-25 17:21:37 +0800

    Update 3-3 : 349. Intersection of Two Arrays.cpp

--- 3. Hash Map/3-3 : 349. Intersection of Two Arrays.cpp
+++ 3. Hash Map/3-3 : 349. Intersection of Two Arrays.cpp
@@ -4,10 +4,6 @@ public:
         unordered_set<int> result;
         unordered_set<int> nums_set(nums1.begin(), nums1.end());
 
-        for(int nums1: nums2){
-            
-        }
-        
         for (int num : nums2) { // num 遍歷 nums2的元素
             if (nums_set.find(num) != nums_set.end()) { // find(某元素）, nums_set.end() 查找到最後
                 result.insert(num); // 插入在最前面
